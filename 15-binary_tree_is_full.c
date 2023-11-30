eaa8ae9cbeaf7adc041e34cd87a6ef59ad77e9f6
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-01 01:11:37 +0200

    Format

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,20 +9,20 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
 
-    if (tree->left && tree->right)
-    {
-        left = binary_tree_is_full(tree->left);
-        right = binary_tree_is_full(tree->right);
-        if (left == 0 || right == 0)
-        {
-            return (0);
-        }
-        return (1);
-    }
-    else if (!tree->left && !tree->right)
-    {
-        return (1);
-    }
+	if (tree->left && tree->right)
+	{
+		left = binary_tree_is_full(tree->left);
+		right = binary_tree_is_full(tree->right);
+		if (left == 0 || right == 0)
+		{
+			return (0);
+		}
+		return (1);
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
 
-    return (0);
+	return (0);
 }
