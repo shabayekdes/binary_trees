a7043f37ca9cffe4ef74770cdbd7f92c7c1304cc
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-01 00:47:32 +0200

    Add binary_tree_size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t right = 0, left = 0;
+
+	if (tree == NULL)
+		return (0);
+
+    left = binary_tree_size(tree->left);
+    right = binary_tree_size(tree->right);
+
+	return (right + left + 1);
+}
