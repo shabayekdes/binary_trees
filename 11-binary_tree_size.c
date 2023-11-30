8088623eb4e6317e97b4f4c6e5db3c360ee750c4
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-01 00:52:24 +0200

    Add binary_tree_leaves

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,8 +11,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-    left = binary_tree_size(tree->left);
-    right = binary_tree_size(tree->right);
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
 
 	return (right + left + 1);
 }
